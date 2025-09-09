15f792d28ee1af9b518e5cba1713c3ae0d817987
Young <liyang_email@163.com>
2025-09-09 08:52:58 +0800

    feat: 调整为Docusaurus框架

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'Young`s notes',
+  title: 'My Site',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
